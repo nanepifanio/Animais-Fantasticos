763fdadfa63ff2e6ebac1f25ff5619036dba3eaf
Renan Epifânio <renan.epifanio@outlook.com>
2021-10-31 19:58:05 -0300

    Bitcoin Fetch refactored

--- javascript/fetch-bitcoin.js
+++ javascript/fetch-bitcoin.js
@@ -1,16 +1,16 @@
-export default function initFetchBitcoin() {
-  function fetchBitcoin() {
-    fetch("https://blockchain.info/ticker")
+export default function fetchBitcoin(url, target) {
+  function setBTCValue(preco) {
+    const bitcoinValue = document.querySelector(target);
+    const doe1000Reais = 1000 / preco;
+    bitcoinValue.innerText = `${doe1000Reais.toFixed(5).replace(".", ",")}`;
+  }
+
+  function bitcoinFetch() {
+    fetch(url)
       .then((responseBitcoin) => responseBitcoin.json())
       .then((bitcoinJSON) => setBTCValue(bitcoinJSON.BRL.buy))
       .catch((error) => console.log(Error(error)));
   }
 
-  fetchBitcoin();
-
-  function setBTCValue(preco) {
-    const bitcoinValue = document.querySelector(".btc-preco");
-    const doe1000Reais = 1000 / preco;
-    bitcoinValue.innerText = `${doe1000Reais.toFixed(5).replace(".", ",")}`;
-  }
+  return bitcoinFetch();
 }
