59b130ead04b411a88c071a120b6834d70574e77
Renan Epifânio <renan.epifanio@outlook.com>
2021-10-31 19:10:08 -0300

    AnimaNumeros refactored

--- javascript/anima-numeros.js
+++ javascript/anima-numeros.js
@@ -1,31 +1,47 @@
-export default function initAnimaNumeros() {
-  function animaNumeros() {
-    const numeros = document.querySelectorAll("[data-numero]");
-
-    numeros.forEach((numero) => {
-      const total = +numero.innerText;
-      const incremento = total / 100;
-      let start = 0;
-      const timer = setInterval(() => {
-        start += incremento;
-        numero.innerText = start.toFixed(0);
-        if (start > total) {
-          numero.innerText = total;
-          clearInterval(timer);
-        }
-      }, 25 * Math.random());
-    });
+export default class AnimaNumeros {
+  constructor(numeros, observeTarget, observerClass) {
+    this.numeros = document.querySelectorAll(numeros);
+    this.observeTarget = document.querySelector(observeTarget);
+    this.observerClass = observerClass;
+    this.handleMutation = this.handleMutation.bind(this);
   }
 
-  function handleMutation(mutation) {
-    if (mutation[0].target.classList.contains("ativo")) {
-      observer.disconnect();
-      animaNumeros();
+  static incrementarNumero(numero) {
+    const total = +numero.innerText;
+    const incremento = total / 100;
+    let start = 0;
+    const timer = setInterval(() => {
+      start += incremento;
+      numero.innerText = start.toFixed(0);
+      if (start > total) {
+        numero.innerText = total;
+        clearInterval(timer);
+      }
+    }, 25 * Math.random());
+  }
+
+  animaNumeros() {
+    this.numeros.forEach((numero) =>
+      this.constructor.incrementarNumero(numero)
+    );
+  }
+
+  handleMutation(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
+      this.observer.disconnect();
+      this.animaNumeros();
     }
   }
 
-  const observeTarget = document.querySelector(".numeros");
-  const observer = new MutationObserver(handleMutation);
+  addObserverListener() {
+    this.observer = new MutationObserver(this.handleMutation);
+    this.observer.observe(this.observeTarget, { attributes: true });
+  }
 
-  observer.observe(observeTarget, { attributes: true });
+  init() {
+    if (this.numeros.length && this.observeTarget) {
+      this.addObserverListener();
+    }
+    return this;
+  }
 }
--- javascript/fetch-animais.js
+++ javascript/fetch-animais.js
@@ -1,4 +1,4 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import AnimaNumeros from "./anima-numeros.js";
 
 export default function initFetchAnimais() {
   async function fetchAnimais() {
@@ -11,7 +11,12 @@ export default function initFetchAnimais() {
           const divAnimal = createAnimals(animal);
           numerosGrid.appendChild(divAnimal);
         });
-        initAnimaNumeros();
+        const animaNumeros = new AnimaNumeros(
+          "[data-numero]",
+          ".numeros",
+          "ativo"
+        );
+        animaNumeros.init();
       }
     } catch (error) {
       console.log(Error(error));
