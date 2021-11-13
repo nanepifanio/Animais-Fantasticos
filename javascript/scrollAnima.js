d0cc875e9d61fdf1b8028547c5b6f77215877a0d
Renan Epifânio <renan.epifanio@outlook.com>
2021-11-12 21:13:53 -0300

    aumento perfomance scroll animation

--- javascript/scrollAnima.js
+++ javascript/scrollAnima.js
@@ -2,29 +2,44 @@ export default class ScrollAnima {
   constructor(sections, cls) {
     this.sections = document.querySelectorAll(sections);
     this.activeClass = cls;
-    this.scrollAnimate = this.scrollAnimate.bind(this);
-    this.windowTop = window.scrollY + window.innerHeight * 0.6;
+    this.checkDistance = this.checkDistance.bind(this);
+    this.windowTop = window.innerHeight * 0.6;
   }
 
-  scrollAnimate() {
-    this.sections.forEach((section) => {
-      if (this.windowTop > section.offsetTop) {
-        section.classList.add(this.activeClass);
-      } else if (section.classList.contains(this.activeClass)) {
-        section.classList.remove(this.activeClass);
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowTop),
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.scrollY > item.offset) {
+        item.element.classList.add(this.activeClass);
+      } else if (item.element.classList.contains(this.activeClass)) {
+        item.element.classList.remove(this.activeClass);
       }
     });
   }
 
   scrollEventListener() {
-    window.addEventListener("scroll", this.scrollAnimate);
+    window.addEventListener("scroll", this.checkDistance);
   }
 
   init() {
     if (this.sections.length) {
-      this.scrollAnimate();
+      this.getDistance();
+      this.checkDistance();
       this.scrollEventListener();
     }
     return this;
+  }
+
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
